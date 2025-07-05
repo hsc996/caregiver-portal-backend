520a0c77d45f9654d95939b16dc382b8b4687895
hsc996 <han.scaife@gmail.com>
2025-07-05 15:20:41 +0100

    journal data seeded successfully

--- src/db/seed/seedDb.js
+++ src/db/seed/seedDb.js
@@ -3,11 +3,19 @@ require('dotenv').config();
 const { dbConnect } = require("../../db/dbFunctions");
 
 const { seedUsers } = require("./seedUsers");
+const { seedJournalEntries } = require("./seedJournalEntries");
+const { UserModel } = require('../../models/userModel');
 
 async function runSeeds(){
     try {
         await dbConnect();
         await seedUsers();
+        
+        const users = await UserModel.find({});
+        const userIds = users.map(u => u._id);
+
+        await seedJournalEntries(userIds);
+
         console.log("All fields seeded successfully.");
     } catch (error) {
         console.log("An error occurred during seeding: " + error);
